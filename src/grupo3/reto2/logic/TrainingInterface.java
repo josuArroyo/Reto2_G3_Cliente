49ab7a8590223115b8af4317e89d937dc39b3571
poker <poker@DESKTOP-OO771KJ>
2023-01-25 15:01:14 +0100

    commit de prueba

--- src/grupo3/reto2/logic/TrainingInterface.java
+++ src/grupo3/reto2/logic/TrainingInterface.java
@@ -11,8 +11,8 @@ import grupo3.reto2.exception.ReadException;
 import grupo3.reto2.exception.UpdateException;
 import grupo3.reto2.model.Entrenamiento;
 import java.util.List;
-//import javax.ws.rs.ClientErrorException;
-//import javax.ws.rs.core.GenericType;
+import javax.ws.rs.ClientErrorException;
+import javax.ws.rs.core.GenericType;
 
 /**
  *
@@ -23,37 +23,36 @@ public interface TrainingInterface {
     
 
     public <T> T findObjetivo_XML(Class<T> responseType, String idObjetivo) throws ClientErrorException;
-//
+
     public <T> T findObjetivo_JSON(Class<T> responseType, String idObjetivo) throws ClientErrorException;
-//     
-//  
-//
-//    public void edit_XML(Object requestEntity) throws ClientErrorException ;
-//
-//    public void edit_JSON(Object requestEntity) throws ClientErrorException ;
-//    
-//
-//    public <T> T findById_XML(Class<T> responseType, String idEntrenamiento) throws ClientErrorException ;
-//
-//    public <T> T findById_JSON(Class<T> responseType, String idEntrenamiento) throws ClientErrorException ;
-//
-//    public <T> T findDuracion_XML(Class<T> responseType, String duracion) throws ClientErrorException;
-//
-//    public <T> T findDuracion_JSON(Class<T> responseType, String duracion) throws ClientErrorException;
-//
-//    public void create_XML(Object requestEntity) throws ClientErrorException ;
-//    
-//    public void create_JSON(Object requestEntity) throws ClientErrorException ;
-//
-//    public <T> T findIntensidad_XML(GenericType<T> responseType, String intensidad) throws ClientErrorException;
-//    
-//    public <T> T findIntensidad_JSON (GenericType<T> responseType, String intensidad) throws ClientErrorException;
-//
-//    public <T> T findAll_XML(GenericType<T> responseType) throws ClientErrorException;
-//
-//    public <T> T findAll_JSON(GenericType<T> responseType) throws ClientErrorException;
-//
-//    public void remove(String id) throws ClientErrorException;
+     
+  
+
+    public void edit_XML(Object requestEntity) throws ClientErrorException ;
+    public void edit_JSON(Object requestEntity) throws ClientErrorException ;
+    
+
+    public <T> T findById_XML(Class<T> responseType, String idEntrenamiento) throws ClientErrorException ;
+
+    public <T> T findById_JSON(Class<T> responseType, String idEntrenamiento) throws ClientErrorException ;
+
+    public <T> T findDuracion_XML(Class<T> responseType, String duracion) throws ClientErrorException;
+
+    public <T> T findDuracion_JSON(Class<T> responseType, String duracion) throws ClientErrorException;
+
+    public void create_XML(Object requestEntity) throws ClientErrorException ;
+    
+    public void create_JSON(Object requestEntity) throws ClientErrorException ;
+
+    public <T> T findIntensidad_XML(GenericType<T> responseType, String intensidad) throws ClientErrorException;
+    
+    public <T> T findIntensidad_JSON (GenericType<T> responseType, String intensidad) throws ClientErrorException;
+
+    public <T> T findAll_XML(GenericType<T> responseType) throws ClientErrorException;
+
+    public <T> T findAll_JSON(GenericType<T> responseType) throws ClientErrorException;
+
+    public void remove(String id) throws ClientErrorException;
 
    
 }
